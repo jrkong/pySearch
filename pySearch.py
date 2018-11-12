fde12de7746f5357e3883318cc1a53ec9e531d78
Josh Mayers <wwedabest@Josh-2.local>
2018-11-12 10:12:47 -0500

    Original pySearch.py file, with encoded characters

--- pySearch.py
+++ pySearch.py
@@ -6,7 +6,6 @@ argparser = argparse.ArgumentParser()
 argparser.add_argument("-s", action="append", help="Takes a query to search for and searches it.", nargs="*", required=True)
 argparser.add_argument("-e", "--engine", help="Changes the name or alias of a search engine and sets it as the search engine for the session", nargs="+")
 argparser.add_argument("-d", "--domain", help="Changes the domain extention", nargs="+")
-argparser.add_argument("-b", "--browser", help="Changes browser to perform search in", nargs="+")
 
 args = argparser.parse_args()
 
